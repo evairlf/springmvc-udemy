838ebf4c47fa83e448744361d9184e02085487b6
Evairlf <evairleitefeldmann@gmail.com>
2021-04-15 22:11:33 -0300

    adcionado classe dominio de produtos e sua interface repository

--- src/main/java/com/feldmann/cursodespring/CursomcApplication.java
+++ src/main/java/com/feldmann/cursodespring/CursomcApplication.java
@@ -8,13 +8,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.feldmann.cursodespring.domain.Categoria;
+import com.feldmann.cursodespring.domain.Produto;
 import com.feldmann.cursodespring.repositories.CategoriaRepository;
+import com.feldmann.cursodespring.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner{
 	
 	@Autowired
 	private CategoriaRepository categoriarepository;
+	
+	@Autowired
+	private ProdutoRepository produtorepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -25,7 +30,21 @@ public class CursomcApplication implements CommandLineRunner{
 		Categoria cat1 = new Categoria(null,"Informatica");
 		Categoria cat2 = new Categoria(null,"Escritório");
 		
+		Produto p1 = new Produto(null,"Computador",2000.00);
+		Produto p2 = new Produto(null,"Impressora",800.00);
+		Produto p3 = new Produto(null,"Mouse",90.00);
+		
+		cat1.getProdutos().addAll(Arrays.asList(p1,p2,p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+		
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1,cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+		
+		
+		
 		categoriarepository.saveAll(Arrays.asList(cat1, cat2));
+		produtorepository.saveAll(Arrays.asList(p1,p2,p3));
 		
 	}
 
