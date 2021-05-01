dd02bb82481395249bf149c7ea08d392648cef77
evairlf <evairleitefeldmann@gmail.com>
2021-05-01 15:05:12 -0300

    Adcionado erros personalizados e lista de erros

--- src/main/java/com/feldmann/cursodespring/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/feldmann/cursodespring/resources/exceptions/ResourceExceptionHandler.java
@@ -2,14 +2,15 @@ package com.feldmann.cursodespring.resources.exceptions;
 
 import javax.servlet.http.HttpServletRequest;
 
-import org.hibernate.exception.ConstraintViolationException;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-
 import com.feldmann.cursodespring.services.exceptions.DataIntegrityException;
 import com.feldmann.cursodespring.services.exceptions.ObjectNotFoundException;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
 
 @ControllerAdvice
@@ -29,11 +30,16 @@ public class ResourceExceptionHandler {
 		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(er);
 	}
 	
-	@ExceptionHandler(ConstraintViolationException.class)
-	public ResponseEntity<StandardError> dataIntegrity(ConstraintViolationException e,HttpServletRequest request){
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<StandardError> validationException(MethodArgumentNotValidException e,HttpServletRequest request){
 		
-		StandardError er = new StandardError(HttpStatus.BAD_REQUEST.value(),e.getMessage(),System.currentTimeMillis());
-		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(er);
+		ValidationError err = new ValidationError(HttpStatus.BAD_REQUEST.value(),"Erro de Validação",System.currentTimeMillis());
+
+		for(FieldError x:  e.getBindingResult().getFieldErrors()){
+			err.addError(x.getField(), x.getDefaultMessage());
+		}
+
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
 	}
 	
 }
